3d3dd8a05824286667892b5fca35cc5b3dec8e60
«LEONID» <nikonovperm@mail.ru>
2022-03-01 20:42:06 +0500

    Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Я так ждала тебя, Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
